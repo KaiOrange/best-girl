6903c4d26846872a025efa8b333e3c7c2b06a0ea
Huang Kai <khuang97@best-inc.com>
2019-01-21 15:31:01 +0800

    build

---
+++ build/precache-manifest.e64578d88d8971b01cd491c0b1b328af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c41542e07648518c6c6e",
+    "url": "./build/static/js/runtime~main.c41542e0.js"
+  },
+  {
+    "revision": "a7da77cac2d67b275a17",
+    "url": "./build/static/js/main.a7da77ca.chunk.js"
+  },
+  {
+    "revision": "8f23e7d313dbf9256e53",
+    "url": "./build/static/js/1.8f23e7d3.chunk.js"
+  },
+  {
+    "revision": "a7da77cac2d67b275a17",
+    "url": "./build/static/css/main.7fd68bd4.chunk.css"
+  },
+  {
+    "revision": "4d6bddff22f4179b81c97af6ff180ad7",
+    "url": "./build/index.html"
+  }
+];
