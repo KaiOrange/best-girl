42e1d73a7b2e5967b44e91a5213d5d4ee9235273
Huang Kai <khuang97@best-inc.com>
2019-01-22 16:14:50 +0800

    build

---
+++ build/precache-manifest.e383a7732cac3d9b7dd6ea484cb43856.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8d99e385377386ad24d",
+    "url": "/best-girl/static/js/runtime~main.f8d99e38.js"
+  },
+  {
+    "revision": "91b0bb7f65a90ad64f91",
+    "url": "/best-girl/static/js/main.91b0bb7f.chunk.js"
+  },
+  {
+    "revision": "4f0aeb203a414f47365b",
+    "url": "/best-girl/static/js/1.4f0aeb20.chunk.js"
+  },
+  {
+    "revision": "91b0bb7f65a90ad64f91",
+    "url": "/best-girl/static/css/main.a9955a69.chunk.css"
+  },
+  {
+    "revision": "a5ef1e76cbb4e7d21babe5588d6d75f4",
+    "url": "/best-girl/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/best-girl/precache-manifest.1f98688aad9e257e2f595dbb4f465d4f.js"
+  "/best-girl/precache-manifest.e383a7732cac3d9b7dd6ea484cb43856.js"
 );
 
 workbox.clientsClaim();
