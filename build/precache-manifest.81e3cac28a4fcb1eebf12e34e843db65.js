2efffe055b6a6f0fcc0fe2b5b1884e051a41acae
KaiOrange <huangkaiking@126.com>
2019-02-16 14:06:39 +0800

    build

---
+++ build/precache-manifest.81e3cac28a4fcb1eebf12e34e843db65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8937abb667188b7f7766",
+    "url": "/static/js/main.8937abb6.chunk.js"
+  },
+  {
+    "revision": "e6eb96ee1e54ee6d5415",
+    "url": "/static/js/1.e6eb96ee.chunk.js"
+  },
+  {
+    "revision": "8937abb667188b7f7766",
+    "url": "/static/css/main.29075e69.chunk.css"
+  },
+  {
+    "revision": "f9ed5eace899f0c939125ef8ff9fd26e",
+    "url": "/index.html"
+  }
+];
