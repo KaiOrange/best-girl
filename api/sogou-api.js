70a84eba2beec25ef8c81458e5673402159d9cc0
Orange <huangkaiking@126.com>
2023-04-27 18:48:33 +0800

    修改文件

--- api/sogou-api.js
+++ api/sogou-api.js
@@ -2,10 +2,20 @@ const { default: axios } = require('axios');
 
 export default async function handler(request, response) {
 
-  let res = await axios({
-    url: 'https://pic.sogou.com/napi/pc/searchList?' + request.url.replace('/api/sogou-api', ''),
-    method: 'GET',
-  });
+  let res = {
+    data: {
+      items: []
+    }
+  }
+  try {
+    res = await axios({
+      url: 'https://pic.sogou.com/napi/pc/searchList?' + request.url.replace('/api/sogou-api', ''),
+      method: 'GET',
+    });  
+  } catch (error) {
+    return response.status(500).send(error);
+  }
+  
 
   return response.status(200).send(res.data);
 }
